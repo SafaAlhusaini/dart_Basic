66e93cde376db0bd04cd23bbbda3331715a9e507
Safa Sulaiman Alhussaini <111584212+SafaAlhusaini@users.noreply.github.com>
2023-08-27 22:17:21 +0400

    Initial commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,109 @@
+import 'dart:html';
+import 'dart:svg';
+
+void main() {
+  //print(123);
+  // print("Dart Course");
+  // print('Dart Course');// Single quote its same to double in dart
+  // print(true);
+  // print(false);
+  // print(null);
+  // print(12.5);
+//!-----------------------------------------------------------Variable:
+//--INT
+  // int a = 5, b = 6;
+  // int x = 10;
+//--DOUBLE
+  // double y = 10.0;
+
+//--STRING
+  // String s = "Safm";
+  // print(s.length);
+  // print(s[0] + " " + s[1] + " " + s[2] + " " + s[3]);
+  //print(s.isEmpty);
+  //print(s.isNotEmpty);
+  //print(s.contains("fa"));
+  //print(s.endsWith("fa"));
+  //print(s.Startswith("Sa"));
+  //print(s.indexOf("f"));
+  // print(s.split("f"));
+  //print(s.trim());
+  // print(s.substring(1,3));
+
+//--PRINT OPERATION WITH STRINGS------ConCatination>
+  // int d = 5;
+  // print("Safa $d");
+  // print("result = ${10 + 5}");
+
+//--EXERCISE------>
+
+  //   double x = 5.5;
+  //   int x2 = 5;
+  //   String word = "Nice";
+  // print(word[0] + " $x2 " + word[1] + " " + word[2] + " $x2 " + word[3]);
+
+//---BOOLEAN------->
+
+  // bool isMarried = false;
+  // print(isMarried);
+
+//----VAR------->
+
+  //var x = 5; //int
+  //dynamic x = "";
+  //x = 5;
+
+//------DATETIME---->
+  // DateTime date;
+  // DateTime date = DateTime(2023, 8, 20);
+  // DateTime date = DateTime.now(); //current time
+  // print(date.day);// reach the day
+  // print(date.year);//reach the year
+
+//------RELATIONAL OPERATOR----->
+  // int x = 10;
+  // int y = 5;
+  // print(x is double);
+
+//------LOGICAL OPERATOR------->
+  // int x = 10;
+  // int y = 5;
+  // print(x < y && x == y);
+  // print(x < y || x == y);
+  // print(x < y || (x == y || x != y));
+  // print(x < y && x == y || x != y);
+  // print(x > y || x < y && x < y && x == y || x != y);
+  // print(x < y || x < y && x < y && x == y || x != y);
+
+//-------IF-STATMENET-------------->
+  // if (true)
+  // {
+  //   print("True");
+  // }
+  // else
+  //  {
+  //   print("False");
+  // }
+
+  // if (true)
+  // {
+  //   print("True");
+  // }
+  // else if
+  //  {
+  //   print("x > 1");
+  // }
+  // else{
+  //   print("false");
+  // }
+
+  //-----SWITCH CASE-------->
+
+  // switch ("Es") {
+  //   case "ES":
+  //     print("ES");
+  //     break;
+  //   default:
+  //     print("Default");
+  // }
+}
